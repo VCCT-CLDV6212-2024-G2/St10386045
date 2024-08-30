c1cfc549a754372c0afdcf74c63612089f9d2c6c
Zuwarashe <zuwarashem1998@gmail.com>
2024-08-30 20:58:29 +0200

    final push

---
+++ CLDV_SemesterTwo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Zuwarashe_Makarimayi_st10386045.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CLDV_SemesterTwo/Services/TableService.cs
@@ -0,0 +1,25 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System.Threading.Tasks;
+using Zuwarashe_Makarimayi_st10386045.Models;
+
+namespace Zuwarashe_Makarimayi_st10386045.Services
+{
+    public class TableService
+    {
+        private readonly TableClient _tableClient;
+
+        public TableService(IConfiguration configuration)
+        {
+            var connectionString = configuration["AzureStorage:ConnectionString"];
+            var serviceClient = new TableServiceClient(connectionString);
+            _tableClient = serviceClient.GetTableClient("CustomerProfiles");
+            _tableClient.CreateIfNotExists();
+        }
+
+        public async Task AddEntityAsync(CustomerProfile profile)
+        {
+            await _tableClient.AddEntityAsync(profile);
+        }
+    }
+}
